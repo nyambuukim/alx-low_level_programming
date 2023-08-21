2e76b2108a29e935a4010fed6b9a06d6336860c8
nyambuukim <nyambuukim1@gmail.com>
2023-08-21 07:15:58 -0700

    Length of the string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,18 +1,18 @@
 #include "main.h"
-
 /**
- * _strlen - finds the length of a string
- * @s: string to be counted
- * Return: Length of the string
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
  */
 int _strlen(char *s)
 {
-	int i;
+	int i = 0;
 
 	while (*s != '\0')
 	{
 		i++;
 		s++;
 	}
+
 	return (i);
 }
