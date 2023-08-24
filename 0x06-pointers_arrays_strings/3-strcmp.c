a14459d211bbb17d8faf69f25d3206c8dd4fc28f
nyambuukim <nyambuukim1@gmail.com>
2023-08-24 08:15:03 -0700

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcmp - function that compares two strings
+ * @s1: parameter to be compared
+ * @s2: parameter to be compared
+ * Return: int
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			if (s1[i] < s2[i])
+				return (s1[i] - s2[i]);
+			else if (s2[i] < s1[i])
+				return (s1[i] - s2[i]);
+		}
+	}
+	return (0);
+}
