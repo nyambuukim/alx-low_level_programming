bd6b23bebe6137f3bdfddba4258385c2afeb3efd
nyambuukim <nyambuukim1@gmail.com>
2023-09-13 09:34:16 -0700

    function that performs simple operations

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -11,10 +11,9 @@
  */
 typedef struct op
 {
-    char *op;
-    int (*f)(int a, int b);
+	char *op;
+	int (*f)(int a, int b);
 } op_t;
-
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
