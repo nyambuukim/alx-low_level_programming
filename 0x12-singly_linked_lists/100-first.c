0f086f609910d5b64c45179d1b599aef569aa05a
nyambuukim <nyambuukim1@gmail.com>
2023-09-21 16:50:05 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,6 +6,6 @@ void myCleanupFun(void) __attribute__ ((constructor));
 
 void myCleanupFun(void)
 {
-        printf("You're beat! and yet, you must allow,\n");
-        printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
